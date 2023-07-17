b4c6182abf03e68324d58375cb0979dcac896779
sommrook-42maru <sommrook@gmail.com>
2023-07-18 08:35:44 +0900

    order business logic

--- jpashop/src/main/java/jpabook/jpashop/domain/Category.java
+++ jpashop/src/main/java/jpabook/jpashop/domain/Category.java
@@ -28,10 +28,13 @@ public class Category {
             inverseJoinColumns = @JoinColumn(name = "item_id"))
     private List<Item> items = new ArrayList<>();
 
+    // 자기 자신 Class 를 join column 한다.
     @ManyToOne(fetch = LAZY)
     @JoinColumn(name = "parent_id")
     private Category parent;
 
+    // mappedBy의 기준은 나 자신 (클래스)
+    // 즉, 나를 가리키는 컬럼이 무엇인지 명시하는것. => Category 의 parent 요소가 나 자신을 가리키면 child list 에 포함
     @OneToMany(mappedBy = "parent")
     private List<Category> child = new ArrayList<>();
 
