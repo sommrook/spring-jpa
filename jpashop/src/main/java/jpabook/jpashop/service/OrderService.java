afb18d07925d86b3b4a4b99cf699735dd9906250
sommrook-42maru <sommrook@gmail.com>
2023-07-19 08:33:30 +0900

    order service

--- jpashop/src/main/java/jpabook/jpashop/service/OrderService.java
+++ jpashop/src/main/java/jpabook/jpashop/service/OrderService.java
@@ -12,6 +12,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+
 @Service
 @RequiredArgsConstructor
 @Transactional(readOnly = true)
@@ -42,12 +44,26 @@ public class OrderService {
         Order order = Order.createOrder(member, delivery, orderItem);
 
         //주문 저장 => 하나만 저장을 해도 Order class 의 cascade 옵션으로 인해 OrderItem, Delivery 둘 다 저장이 된다.
+        // cascade 를 사용한 클래스가 기준이 된다. (Order)
         orderRepository.save(order);
 
         return order.getId();
     }
 
-    //취소
+    /**
+     * 주문 취소
+     */
+    @Transactional
+    public void cancelOrder(Long orderId){
+        //주문 엔티티 조회
+        Order order = orderRepository.findOne(orderId);
+        //주문 조회
+        order.cancel();
+    }
 
     //검색
+    // ctrl + shift + /
+    /*public List<Order> findOrders(OrderSearch orderSearch){
+        return orderRepository.findAll(orderSearch);
+    }*/
 }
